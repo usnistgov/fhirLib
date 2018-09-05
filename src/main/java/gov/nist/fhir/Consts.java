b776685c50c691342eadbab7ba173e1fd542ae96
Andrew McCaffrey <andrew.mccaffrey@nist.gov>
2018-09-05 14:44:06 -0400

    Imported new TCH code and expanded for connection via HL7/IIS

--- src/main/java/gov/nist/fhir/Consts.java
+++ src/main/java/gov/nist/fhir/Consts.java
@@ -15,6 +15,9 @@ public final class Consts {
     public static final String PARAMETER_NAME_BIRTH_DATE = "birthDate";
     public static final String PARAMETER_NAME_SERVICE_TYPE = "serviceType";
     public static final String PARAMETER_NAME_SERVICE_URL = "serviceURL";
+    public static final String PARAMETER_NAME_USER_ID = "userID";
+    public static final String PARAMETER_NAME_FACILITY_ID = "facilityID";
+    public static final String PARAMETER_NAME_PASSWORD = "password";
     public static final String PARAMETER_NAME_ASSESSMENT_DATE = "assessmentDate";
     public static final String PARAMETER_NAME_IMMUNIZATION = "Immunization";
     public static final String PARAMETER_NAME_IMMUNIZATION_ADAPTER = "Immunizations";
